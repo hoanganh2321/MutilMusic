e20757b72e2a1bafc0f778eb6a45367c2c1ae3f4
hoanganh2321 <131464214+hoanganh2321@users.noreply.github.com>
2024-05-14 09:57:24 +0700

    Update owner.js

--- utilityCommands/owner.js
+++ utilityCommands/owner.js
@@ -4,12 +4,12 @@ module.exports = {
     name: 'owner',
     description: 'Bot owner info',
     execute(message, args) {
-        const youtubeLink = 'https://www.youtube.com/@GlaceYT';
-        const InstagramLink = 'https://www.instagram.com/glaceytt';
+        const facebookLink = 'https://www.facebook.com/Cheezze221/';
+        const discordLink = 'https://discord.gg/XBHZUwqAzK';
         const embed = new EmbedBuilder()
             .setColor('#FFFFFF')
             .setTitle(' 🫅 Owner Info')
-            .setDescription(`__**About me**__:\n 🤖 Myself Shiva aka GlaceYT. I am a discord bot developer and web developer. I love playing games, watching anime and building different webserver applications. You will get faster replies on instagram than other social media. Feel free to contact me!\n ❤️ [GlceYT](${youtubeLink})\n 💙 [GlaceYTT](${InstagramLink})`)
+            .setDescription(`__**About me**__:\n 🤖 Myself Hoàng Anh aka Kaioshin. I am a discord bot developer and web developer. I love playing games, watching anime and building different webserver applications. You will get faster replies on instagram than other social media. Feel free to contact me!\n ❤️ [Hoang Anhh](${facebookLink})\n 💙 [Discord](${discordLink})`)
             .setTimestamp();
 
 
