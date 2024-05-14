92fea35d62bdb69902b3ca693621d37eb67ccc35
hoanganh2321 <131464214+hoanganh2321@users.noreply.github.com>
2024-05-14 09:38:43 +0700

    Update help.js

--- basicCommands/help.js
+++ basicCommands/help.js
@@ -14,10 +14,10 @@ module.exports = {
       .setAuthor({
         name: 'Im here to Help!',
         iconURL: 'https://cdn.discordapp.com/attachments/1175487983915376662/1175667506791325706/communication.png?ex=656c10b0&is=65599bb0&hm=e378f1b355a2401bcab504b08a0766001d6b7c090c91ce0a7a7a87c868feb955&', 
-        url: 'https://discord.gg/FUEHs7RCqz'
+        url: 'https://discord.gg/XBHZUwqAzK'
     })
      
-      .setDescription(`__**STATS :**__\n\n> **📊 Bot in servers:** ${serverCount}\n> **🟢 Bot Ping:** ${botPing}ms\n> **👑 Made By [GlaceYT](https://www.youtube.com/channel/UCPbAvYWBgnYhliJa1BIrv0A)**\n\n__**COMMANDS :**__ `)
+      .setDescription(`__**STATS :**__\n\n> **📊 Bot in servers:** ${serverCount}\n> **🟢 Bot Ping:** ${botPing}ms\n> **👑 Made By [Kaioshin](https://www.facebook.com/Cheezze221/)**\n\n__**COMMANDS :**__ `)
       .addFields(
       
         {
@@ -46,7 +46,7 @@ module.exports = {
       
         {
           name: '▶️  Anime',
-          value: '`<prefix>animecommands for more info`',
+          value: '`?animecommands for more info`',
           inline: true,
         },
      
@@ -57,25 +57,22 @@ module.exports = {
         }
       )
       .setThumbnail(botUser.avatarURL({ dynamic: true, format: 'png', size: 1024 }))
-      .setImage(`https://cdn.discordapp.com/attachments/1113800537402527903/1236803979996958740/11.png?ex=6641e8b7&is=66409737&hm=96f69ba8bcde4ca44bf8713e22001e0bafad0335cfd1f57c439a34b4762f0e03&`);
+      .setImage(`https://cdn.discordapp.com/attachments/1239531732512800879/1239768688224108554/taoanhdep_cover_glitch_44024.png?ex=66442011&is=6642ce91&hm=f5c6b41c2d28c43e5ce4b06cb8caaf094053622786286c6845238e4616a2b9a1&`);
 
     const button1 = new ButtonBuilder()
-      .setLabel('YouTube')
-      .setURL('https://www.youtube.com/channel/UCPbAvYWBgnYhliJa1BIrv0A')
+      .setLabel('Facebook')
+      .setURL('https://www.facebook.com/Cheezze221/')
       .setStyle(ButtonStyle.Link);
 
     const button2 = new ButtonBuilder()
       .setLabel('Discord')
-      .setURL('https://discord.gg/xQF9f9yUEM')
+      .setURL('https://discord.gg/XBHZUwqAzK')
       .setStyle(ButtonStyle.Link);
 
-    const button3 = new ButtonBuilder()
-      .setLabel('Code')
-      .setURL('https://github.com/GlaceYT')
-      .setStyle(ButtonStyle.Link);
+
       
     const row = new ActionRowBuilder()
-      .addComponents(button1, button2, button3);
+      .addComponents(button1, button2, );
     
     message.reply({ embeds: [embed], components: [row] });
   },
